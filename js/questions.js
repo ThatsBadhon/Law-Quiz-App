0c5bb63943f662be7a74ca66e679aeda8af94bba
Md. Akhtarujjaman Badhon <91864729+ThatsBadhon@users.noreply.github.com>
2021-12-20 19:51:00 +0600

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "দেওয়ানী কার্যবিধির “বিধি সমূহ” কে পরিবর্তন করিতে পারেন?",
+    answer: "সুপ্রীম কোর্ট",
+    options: [
+      "জাতীয় সংসদ",
+      "সুপ্রীম কোর্ট",
+      "জেলা জজ কোর্ট",
+      "কোনটি নয়"
+    ]
+  },
+    {
+    numb: 2,
+    question: "বাদীর অনুপস্থিতির জন্য মোকদ্দমা খারিজের সিদ্ধান্ত-?",
+    answer: "ডিক্রির অন্তর্ভূক্ত নয়",
+    options: [
+      "ডিক্রির অন্তর্ভূক্ত নয়",
+      "সোলে ডিক্রির অন্তর্ভূক্ত",
+      "প্রাথমিক ডিক্রির অন্তর্ভূক্ত",
+      "চূড়ান্ত ডিক্রির অন্তর্ভূক্ত"
+    ]
+  },
+    {
+    numb: 3,
+    question: "নিচের কোন মামলাটি দেওয়ানী প্রকৃতির মামলা নয়?",
+    answer: "স্বত্ব ঘোষণার মামলা",
+    options: [
+      "বন্টনের মামলা",
+      "স্বত্ব ঘোষণার মামলা",
+      "ডাকাতির মামলা",
+      "জমিতে দখলের মামলা"
+    ]
+  },
+    {
+    numb: 4,
+    question: "ঘোষণামূলক মামলায় প্রতি ঘোষণায় জন্য ফিক্সড কোর্ট ফি কত?",
+    answer: "৩০০ টাকা",
+    options: [
+      "১০০০ টাকা",
+      "২০০ টাকা",
+      "৩০০ টাকা",
+      "Ad-valorem"
+    ]
+  },
+    {
+    numb: 5,
+    question: "বাধ্যতামূলক নিষেধাজ্ঞা সম্পর্কে সুনির্দিষ্ট প্রতিকার আইনের কত ধারায় বর্ণিত আছে?",
+    answer: "কোনটি নয়",
+    options: [
+      "অন্তবর্তীকালীন নিষেধাজ্ঞা",
+      "চিরস্থায়ী নিষেধাজ্ঞা",
+      "অস্থায়ী নিষেধাজ্ঞা",
+      "কোনটি নয়"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
